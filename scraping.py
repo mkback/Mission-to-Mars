d3a20c1c951f58a6e5cf100841d0e462d8da7216
Maddie Back <maddie.back@gmail.com>
2021-11-03 12:25:14 -0700

    challenge

--- scraping.py
+++ scraping.py
@@ -22,7 +22,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemisphere": hemispheres(browser)
     }
 
     # Stop webdriver and return data
@@ -100,6 +101,27 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
+
+def hemispheres(browser): 
+    
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+    
+    hemisphere_image_urls = []
+    
+    for i in range(4):
+        hemispheres = {}
+        browser.find_by_css('a.product-item h3')[i].click()
+        element = browser.find_link_by_text('Sample').first
+        img_url = element['href']
+        title = browser.find_by_css("h2.title").text
+        hemispheres["img_url"]= img_url
+        hemispheres["title"]= title
+        hemisphere_image_urls.append(hemispheres)
+        browser.back()
+    return hemisphere_image_urls
+    
+    
 if __name__ == "__main__":
 
     # If running as script, print scraped data
